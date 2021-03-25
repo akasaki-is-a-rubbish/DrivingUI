6bedf7cb569b24ceb10756c8c594549b2cd84bc6
lideming <me@yuuza.net>
2021-03-25 20:34:19 +0800

    minor

--- src/FrontActivity.tsx
+++ src/FrontActivity.tsx
@@ -1,27 +1,36 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef } from 'react';
 import { Activity } from './Basics';
 import { Client } from './Client';
 import { useWebfxCallback, useWebfxRef } from './utils';
 
 export function FrontActivity(props: { hidden: boolean; }) {
     const canvas = useRef<HTMLCanvasElement>(null);
-    const {w=0, h=0} = useWebfxRef(Client.current.getData('image')) || {};
-    useWebfxCallback(Client.current.onReceivedBinary, async (data) => {
-        if (props.hidden) return;
-        console.info('rendering image')
+    const {w=1, h=1} = useWebfxRef(Client.current.getData('image')) || {};
+    const render = useMemo(() => {
+        if (!canvas.current) return () => {};
         const ctx = canvas.current!.getContext('2d')!;
         const img = ctx.createImageData(w, h);
         const imgdata = img.data;
-        const buf =  new Uint8Array(await data.arrayBuffer());
-        const pixelCount = buf.length / 3;
-        for (var i = 0; i < pixelCount; i++) {
-            imgdata[4 * i] = buf[3 * i];
-            imgdata[4 * i + 1] = buf[3 * i + 1];
-            imgdata[4 * i + 2] = buf[3 * i + 2];
-            imgdata[4 * i + 3] = 255;
+
+        function render(data: ArrayBuffer) {
+            console.info('rendering image')
+            const buf =  new Uint8Array(data);
+            const pixelCount = buf.length / 3;
+            for (var i = 0; i < pixelCount; i++) {
+                imgdata[4 * i] = buf[3 * i + 2];
+                imgdata[4 * i + 1] = buf[3 * i + 1];
+                imgdata[4 * i + 2] = buf[3 * i + 0];
+                imgdata[4 * i + 3] = 255;
+            }
+            ctx.putImageData(img, 0, 0);
         }
-        ctx.putImageData(img, 0, 0);
-    }, [props.hidden, w, h]);
+        return render;
+    }, [w, h, canvas.current]);
+    useWebfxCallback(Client.current.onReceivedBinary, (data) => {
+        if (!props.hidden) {
+            data.arrayBuffer().then(buf => render(buf));
+        }
+    }, [props.hidden]);
     return (
         <Activity hidden={props.hidden} className="front">
             <canvas width={w} height={h} ref={canvas}></canvas>
