96dbbaf65f2919c6c6795cd6e8b108868dfc8434
lideming <me@yuuza.net>
2021-04-03 22:24:11 +0800

    moved pixel format transform process to the backend

--- src/FrontActivity.tsx
+++ src/FrontActivity.tsx
@@ -44,13 +44,14 @@ export function FrontActivity(props: { hidden: boolean; }) {
         function convertData(data: ArrayBuffer) {
             convTime.begin();
             const buf = new Uint8Array(data);
-            const pixelCount = buf.length / 3;
-            for (var i = 0; i < pixelCount; i++) {
-                imgdata[4 * i] = buf[3 * i];
-                imgdata[4 * i + 1] = buf[3 * i + 1];
-                imgdata[4 * i + 2] = buf[3 * i + 2];
-                imgdata[4 * i + 3] = 255;
-            }
+            imgdata.set(buf);
+            // const pixelCount = buf.length / 3;
+            // for (var i = 0; i < pixelCount; i++) {
+            //     imgdata[4 * i] = buf[3 * i];
+            //     imgdata[4 * i + 1] = buf[3 * i + 1];
+            //     imgdata[4 * i + 2] = buf[3 * i + 2];
+            //     imgdata[4 * i + 3] = 255;
+            // }
             convTime.end();
         }
 
