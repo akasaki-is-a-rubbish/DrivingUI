0527e0cb618158773c7d27161dffc11d809925fd
lideming <me@yuuza.net>
2021-04-03 22:17:25 +0800

    bgr -> rgb

--- src/FrontActivity.tsx
+++ src/FrontActivity.tsx
@@ -46,9 +46,9 @@ export function FrontActivity(props: { hidden: boolean; }) {
             const buf = new Uint8Array(data);
             const pixelCount = buf.length / 3;
             for (var i = 0; i < pixelCount; i++) {
-                imgdata[4 * i] = buf[3 * i + 2];
+                imgdata[4 * i] = buf[3 * i];
                 imgdata[4 * i + 1] = buf[3 * i + 1];
-                imgdata[4 * i + 2] = buf[3 * i + 0];
+                imgdata[4 * i + 2] = buf[3 * i + 2];
                 imgdata[4 * i + 3] = 255;
             }
             convTime.end();
