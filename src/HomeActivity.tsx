5a18a4d2fb4da089197dfc390c386959e7a63b60
lideming <me@yuuza.net>
2021-04-04 18:40:16 +0800

    add Home

---
+++ src/HomeActivity.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Activity } from "./Basics";
+
+function HomeActivity(props: { hidden: boolean; }) {
+    <Activity className="home" hidden={props.hidden}>
+        
+    </Activity>;
+}
