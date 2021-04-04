b356f7afa22abe91c388026c2eedd0fedf81ce2f
VisualDust <Miya@Akasaki.space>
2021-04-04 09:16:16 +0800

    changes on akasakis laptop

--- src/FrontActivity.tsx
+++ src/FrontActivity.tsx
@@ -52,6 +52,7 @@ export function FrontActivity(props: { hidden: boolean; }) {
                 rgba[4 * i] = buf[3 * i];
                 rgba[4 * i + 1] = buf[3 * i + 1];
                 rgba[4 * i + 2] = buf[3 * i + 2];
+                // rgba[4 * i + 3] = 255;
             }
             imgdata.set(rgba, 0);
             convTime.end();
