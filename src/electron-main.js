0eee2834091d6efe059e508ace2efb99371ee200
lideming <me@yuuza.net>
2021-04-08 00:00:23 +0800

    change default window size

--- src/electron-main.js
+++ src/electron-main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1300,
+    height: 750,
     webPreferences: {
       nodeIntegration: false,
     }
