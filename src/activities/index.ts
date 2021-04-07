f37de632c23f7a88491dc8bcdd3167f2e1745fa4
lideming <me@yuuza.net>
2021-04-08 03:27:46 +0800

    change nav items order

--- src/activities/index.ts
+++ src/activities/index.ts
@@ -6,13 +6,13 @@ import { HomeActivity } from './HomeActivity';
 import { FrontActivity } from './FrontActivity';
 
 export const activities = [
-    { key: 'home', friendlyName: '首页', activity: HomeActivity, icon: Dashboard},
     { key: 'front', friendlyName: '行驶', activity: FrontActivity, icon: Cars },
     { key: 'rac', friendlyName: '倒车', activity: RadarAndCamsActivity, icon: Radar },
     // { key: 'lidar', friendlyName: '雷达', activity: LidarActivity, icon: LocationOn },
     { key: 'music', friendlyName: '音乐', activity: MusicActivity, icon: MusicIcon },
+    { key: 'home', friendlyName: '更多', activity: HomeActivity, icon: Dashboard},
 ] as const;
 
 export type ActivityName = (typeof activities)[number]['key'];
 
-export const defaultActivitiy: ActivityName = 'home'; 
+export const defaultActivitiy: ActivityName = 'front'; 
