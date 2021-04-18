2e892a1b8b59bd5ab1ea67cc65729867206e4645
lideming <me@yuuza.net>
2021-04-18 11:25:02 +0800

    start dev server from the electron main process

--- src/electron-main.js
+++ src/electron-main.js
@@ -1,27 +1,39 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const { spawn } = require('child_process');
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 1300,
     height: 750,
     webPreferences: {
       nodeIntegration: false,
     }
-  })
+  });
   win.setMenuBarVisibility(false);
   win.loadURL('http://localhost:3000');
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
+
+app.on('quit', (e) => {
+  devserver.kill();
+});
+
+var devserver = spawn('node', ['node_modules/vite/bin/vite.js'], {
+  stdio: 'pipe',
+  shell: false
+});
+devserver.stdout.pipe(devserver.stdout);
+devserver.stderr.pipe(devserver.stderr);
