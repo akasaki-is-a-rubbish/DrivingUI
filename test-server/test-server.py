e80fbd2b73db49372649cc9d2eb4816d7bc61658
lideming <me@yuuza.net>
2021-05-20 03:08:25 +0800

    comments: test-server

--- test-server/test-server.py
+++ test-server/test-server.py
@@ -1,3 +1,7 @@
+"""
+This is a test server for testing the performance of the rear video rendering.
+"""
+
 import json
 import websockets
 import asyncio
@@ -6,18 +10,30 @@ import cv2
 
 
 async def client_handler(client: websockets.WebSocketServerProtocol, path: str):
+    """
+    The asyncio routine for each established client.
+    There should be excatly one client though.
+    """
+
     print('client connected', client.remote_address)
     try:
+        # Pre-generate the fake image for lower overhead
         img = np.empty((720, 1280, 3), np.uint8)
         # ret, buf = cv2.imencode('*.bmp', img)
         buf = img.tobytes("C")
 
         while True:
+            # Wait for the "request frame" message from the client
             await client.recv();
+
+            # Send the image "header"
             await client.send(json.dumps({'image': {'w': img.shape[1], 'h': img.shape[0]}}))
-            # img = img.tobytes("C")
-            # print(len(buf))
+
+            #
             await client.send(buf)
+
+            # # The sleeping was used to control the framerate before we
+            # # changed the "request frame" protocol.
             # await asyncio.sleep(0.03)
 
     finally:
@@ -26,6 +42,7 @@ async def client_handler(client: websockets.WebSocketServerProtocol, path: str):
 
 
 async def main():
+    # Be sure to disable transport compression of websockets
     server = await websockets.serve(client_handler, '127.0.0.1', 8765, compression=None)
     print('test server started listening...')
     await server.wait_closed()
