54987b3c4c7e9d75828db9b73cb69e226294ea54
lideming <me@yuuza.net>
2021-08-16 09:55:27 +0800

    add pdpd logo

--- src/activities/FrontActivity.tsx
+++ src/activities/FrontActivity.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { Activity, createActivity } from './Activity';
 import { LidarView } from '../components/LidarView';
 import { RearView } from '../components/RearView';
+import pdpd from "../../res/pdpd.png";
 
 export const FrontActivity = createActivity(function (props) {
     return (
         <Activity hidden={props.hidden} className="front">
             <RearView hidden={props.hidden} />
             <LidarView />
+            <img src={pdpd} alt="" style={{
+                position: 'absolute',
+                right: '10px',
+                bottom: '10px',
+                width: '240px',
+                background: 'none',
+                height:'auto',
+                'filter': 'drop-shadow(0 0 3px #000000)'
+            }}/>
         </Activity>
     );
 });
